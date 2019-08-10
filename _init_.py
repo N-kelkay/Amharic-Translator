f5a91ad18afbdb6d0e879a0fda1c95065268b1a9
Nate Kelkay <natikelkay@gmail.com>
2019-08-10 17:39:15 -0400

    added a way to append text to the doc

--- _init_.py
+++ _init_.py
@@ -1,7 +1,19 @@
-file = open("TranslateTestFile.txt", 'r')
+#Author Natnael Kelkay
+
+# "r" Opens a file for reading only.
+# "r+" Opens a file for both reading and writing.
+# "rb" Opens a file for reading only in binary format.
+# "rb+" Opens a file for both reading and writing in binary format.
+# "w" Opens a file for writing only.
+file = open("TranslateTestFile.txt", 'r+')
+
 finalTrans = ""
+lineToAddCounter = 0
+
+file.write("\n\nHere is the final translation:\n")
 
 for line in file:
+    lineTrans = ""
     row = line
     rowList = list(row.split(" "))
     
@@ -27,54 +39,78 @@ for line in file:
                 elif y == "፨": # ፨ paragraph separator 
                     finalTrans += "" #Also not sure about this one
             
-            #the ha family
+            #the ha family ሀ ሁ ሂ ሃ ሄ ህ ሆ
             elif y == "ሀ":
                 finalTrans += "hä"
+                lineTrans += "hä"
             elif y == "ሁ":
                 finalTrans += "hu"
+                lineTrans += "hu"
             elif y == "ሂ":
                 finalTrans += "hi"
+                lineTrans += "hi"
             elif y == "ሃ":
                 finalTrans += "ha"
+                lineTrans += "ha"
             elif y == "ሄ":
                 finalTrans += "hē"
+                lineTrans += "hē"
             elif y == "ህ":
                 finalTrans += "hə"
+                lineTrans += "hə"
             elif y == "ሆ":
                 finalTrans += "ho"
+                lineTrans += "ho"
             
             #the le family   ለ ሉ ሊ ላ ሌ ል ሎ
             elif y == "ለ":
                 finalTrans += "le"
+                lineTrans += "le"
             elif y == "ሉ":
                 finalTrans += "lu"
+                lineTrans += "lu"
             elif y == "ሊ":
                 finalTrans += "li"
+                lineTrans += "li"
             elif y == "ላ":
                 finalTrans += "la"
+                lineTrans += "la"
             elif y == "ሌ":
                 finalTrans += "lē"
+                lineTrans += "lē"
             elif y == "ል":
                 finalTrans += "lə"
+                lineTrans += "lə"
             elif y == "ሎ":
                 finalTrans += "lo"
+                lineTrans += "lo"
 
             #the second ha family ሐ ሑ ሒ ሓ ሔ ሕ ሖ
             elif y == "ሐ":
                 finalTrans += "hhä"
+                lineTrans += "hhä"
             elif y == "ሑ":
                 finalTrans += "hhu"
+                lineTrans += "hhu"
             elif y == "ሒ":
                 finalTrans += "hhi"
+                lineTrans += "hhi"
             elif y == "ሓ":
                 finalTrans += "hha"
+                lineTrans += "hha"
             elif y == "ሔ":
                 finalTrans += "hhē"
+                lineTrans += "hhē"
             elif y == "ሕ":
                 finalTrans += "hhə"
+                lineTrans += "hhə"
             elif y == "ሖ":
                 finalTrans += "hho"
-        finalTrans += " "
+                lineTrans += "hho"
+    finalTrans += " "
+    file.write(lineTrans + "\n")
 
-print("Here is the final translation:")
 print(finalTrans)
+
+
+#file.write(finalTrans)
