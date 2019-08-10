a30de7b41aa9f87d16f08ea84b4b56fa18b02023
Nate Kelkay <natikelkay@gmail.com>
2019-08-10 17:05:30 -0400

    add in punctuations

--- _init_.py
+++ _init_.py
@@ -7,7 +7,27 @@ for line in file:
     
     for x in rowList:
         for y in x:
-            if y == "ሀ":
+            if y == "፠" or y =="፡" or y =="።" or y =="፣" or y =="፤" or y =="፥" or y =="፦" or y =="፧" or y =="፨":
+                if y == "፠": # ፠ section mark
+                    finalTrans += "§"
+                elif y == "፡": # ፡ word separator
+                    finalTrans += "|"
+                elif y == "።": # ። full stop (period)
+                    finalTrans += "."
+                elif y == "፣": # ፣ comma
+                    finalTrans += ","
+                elif y == "፤": # ፤ semicolon
+                    finalTrans += ";"
+                elif y == "፥": # ፥ colon
+                    finalTrans += ":"
+                elif y == "፦": # ፦ Preface colon (introduces speech from a descriptive prefix)
+                    finalTrans += ":" #Not really sure about this one...
+                elif y == "፧": # ፧ question mark
+                    finalTrans += "?"
+                elif y == "፨": # ፨ paragraph separator 
+                    finalTrans += "" #Also not sure about this one
+                finalTrans += "hello"
+            elif y == "ሀ":
                 finalTrans += "hä"
             elif y == "ሁ":
                 finalTrans += "hu"
